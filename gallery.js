ec430705e0eeea85780ca7d9dee08b7f4ce60ec9
GermantasSt <germantas.stasytis@gmail.com>
2018-08-01 16:34:59 +0300

    lightbox done

--- gallery.js
+++ gallery.js
@@ -4,6 +4,7 @@ const burgerMenu = $('.nav-menu');
 const meniu = $('.menu');
 
 const imageArray = [];
+let activeImage = 0;
 
 burger.addEventListener('click', () => {
   // burger.classList.toggle('active');
@@ -16,20 +17,42 @@ function removeImage() {
   document.body.removeChild(lightBoxElement);
 }
 
-function drawLightBox(index) {
+
+function drawLightBox() {
   const lightBoxElement = document.createElement('div');
   const lightImage = document.createElement('img');
-  lightImage.src = imageArray[index];
+  lightImage.src = imageArray[activeImage];
   lightBoxElement.appendChild(lightImage);
   document.body.appendChild(lightBoxElement);
   lightBoxElement.classList.add('visibleImage');
   lightBoxElement.addEventListener('click', () => removeImage());
+  const prevArrow = document.createElement('span');
+  const nextArrow = document.createElement('span');
+  lightBoxElement.appendChild(prevArrow);
+  lightBoxElement.appendChild(nextArrow);
+  prevArrow.classList.add('prev');
+  nextArrow.classList.add('next');
+  prevArrow.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const newIndex = (activeImage <= 0 ? imageArray.length: activeImage) - 1;
+    lightImage.src = imageArray[newIndex];
+    activeImage = newIndex;
+  });
+  nextArrow.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const newIndex = activeImage >= imageArray.length - 1 ? 0: activeImage + 1;
+    lightImage.src = imageArray[newIndex];
+    activeImage = newIndex;
+  });
 }
 
 function lightbox() {
   const selectedImages = document.querySelectorAll('.gallery img');
   selectedImages.forEach((image, index) => {
-    image.addEventListener('click', () => drawLightBox(index));
+    image.addEventListener('click', () => { 
+      activeImage = index;
+      drawLightBox();
+    });
     imageArray.push(image.src);
   });
 
